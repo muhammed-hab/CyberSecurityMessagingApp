b4eb231849dd6f6480bbe715f1f614445e56b5b6
muhammed-hab <72678073+muhammed-hab@users.noreply.github.com>
2024-11-16 20:50:25 -0600

    Bryce's contribution: PasswordHashesImpl. All test cases passed

--- src/main/java/server/PasswordHashesImpl.java
+++ src/main/java/server/PasswordHashesImpl.java
@@ -1,28 +1,40 @@
 package server;
 
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.Base64;
+
 public class PasswordHashesImpl implements PasswordHashes {
     @Override
     public byte[] secureRandom(int size) throws Exception {
-        throw new RuntimeException("Not implemented");
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] randomBytes = new byte[size];
+        secureRandom.nextBytes(randomBytes);
+        return randomBytes;
     }
 
     @Override
     public byte[] saltPassword(byte[] password, byte[] salt) {
-        throw new RuntimeException("Not implemented");
+        byte[] saltedPassword = new byte[password.length];
+        for (int i = 0; i < password.length; i++) {
+            saltedPassword[i] = (byte) (password[i] ^ salt [i % salt.length]);
+        }
+        return saltedPassword;
     }
 
     @Override
     public byte[] hash(byte[] data) throws Exception {
-        throw new RuntimeException("Not implemented");
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        return digest.digest(data);
     }
 
     @Override
     public String encodeBase64(byte[] data) {
-        throw new RuntimeException("Not implemented");
+        return Base64.getEncoder().encodeToString(data);
     }
 
     @Override
     public byte[] decodeBase64(String data) {
-        throw new RuntimeException("Not implemented");
+        return Base64.getDecoder().decode(data);
     }
 }
